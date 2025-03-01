9bc1e4b620049e5675f0f2e149c5ac64f3f90669
freddie-nelson <freddie0208@hotmail.com>
2025-03-01 16:55:44 +0000

    feat: better movement

--- game/src/systems/playerSystem.ts
+++ game/src/systems/playerSystem.ts
@@ -1,7 +1,9 @@
 import { Transform } from "@engine/src/core/transform";
 import { System, SystemType, SystemUpdateData } from "@engine/src/ecs/system";
 import { Vec2 } from "@engine/src/math/vec";
+import { RectangleCollider } from "@engine/src/physics/collider";
 import { Rigidbody } from "@engine/src/physics/rigidbody";
+import { GROUND_GROUP } from "@shared/src/groups";
 import { State } from "@state/src/state";
 
 export class PlayerSystem extends System {
@@ -17,6 +19,38 @@ export class PlayerSystem extends System {
     for (const [id, p] of this.players) {
       if (!registry.has(p.entity)) {
         continue;
+      }
+
+      const rigidbody = registry.get(p.entity, Rigidbody);
+      const transform = registry.get(p.entity, Transform);
+      const collider = registry.get(p.entity, RectangleCollider);
+
+      const groundCollisions = engine.physics.queryRay(
+        transform.position,
+        new Vec2(0, -1),
+        (collider.height / 2) * 1.02
+      );
+      const leftCollisions = engine.physics.queryRay(
+        transform.position,
+        new Vec2(-1, 0),
+        (collider.width / 2) * 1.02
+      );
+      const rightCollisions = engine.physics.queryRay(
+        transform.position,
+        new Vec2(1, 0),
+        (collider.width / 2) * 1.02
+      );
+
+      const isGrounded = groundCollisions.some((c) => c.bodyA.collisionFilter.group === GROUND_GROUP);
+      const isBlockedLeft = leftCollisions.some((c) => c.bodyA.collisionFilter.group === GROUND_GROUP);
+      const isBlockedRight = rightCollisions.some((c) => c.bodyA.collisionFilter.group === GROUND_GROUP);
+
+      if (isGrounded) {
+        Rigidbody.setFrictionAir(rigidbody, 0.4);
+      } else if (isBlockedLeft || isBlockedRight) {
+        Rigidbody.setFrictionAir(rigidbody, 0.6);
+      } else {
+        Rigidbody.setFrictionAir(rigidbody, 0.05);
       }
 
       // const t = registry.get(p.entity, Transform);
