4d5db7216e69e05dfa5bfbd73bf5718641174e8c
BuddyGames04 <cochranel@outlook.com>
2025-03-02 12:02:10 +0000

    3 levels done

--- game/src/maps/index.ts
+++ game/src/maps/index.ts
@@ -1,8 +1,16 @@
 import level0JSON from "./level0.json";
 import level1JSON from "./level1.json";
+import level2JSON from "./level1.json";
+import level3JSON from "./level2.json";
+import level4JSON from "./level3.json";
+
 
 export const levels = [
   level0JSON,
   level1JSON,
+  level2JSON,
+  level3JSON,
+  level4JSON,
+
   // Add more levels here
 ];
