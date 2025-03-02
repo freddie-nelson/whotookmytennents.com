c7170269938bd5d858155957345dc627743d9794
BuddyGames04 <cochranel@outlook.com>
2025-03-02 12:32:44 +0000

    updated index

--- game/src/maps/index.ts
+++ game/src/maps/index.ts
@@ -1,9 +1,9 @@
 import level0JSON from "./level0.json";
 import level1JSON from "./level1.json";
-import level2JSON from "./level1.json";
-import level3JSON from "./level2.json";
-import level4JSON from "./level3.json";
-
+import level2JSON from "./level2.json";
+import level3JSON from "./level3.json";
+import level4JSON from "./level4.json";
+import level5JSON from "./level5.json";
 
 export const levels = [
   level0JSON,
@@ -11,6 +11,6 @@ export const levels = [
   level2JSON,
   level3JSON,
   level4JSON,
-
+  level5JSON
   // Add more levels here
 ];
