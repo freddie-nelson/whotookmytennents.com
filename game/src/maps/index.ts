b39647079c976907b3bf708f0270a2198de0ab97
BuddyGames04 <cochranel@outlook.com>
2025-03-02 12:49:30 +0000

    Time to game

--- game/src/maps/index.ts
+++ game/src/maps/index.ts
@@ -4,6 +4,8 @@ import level2JSON from "./level2.json";
 import level3JSON from "./level3.json";
 import level4JSON from "./level4.json";
 import level5JSON from "./level5.json";
+import level6JSON from "./level6.json";
+
 
 export const levels = [
   level0JSON,
